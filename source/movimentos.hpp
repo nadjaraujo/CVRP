c7366a626403824e544a768dd3242d40b7796604
Nadja Araujo <nadja.karla_@hotmail.com>
2020-03-06 19:04:38 -0300

    movimento/v

--- source/movimentos.hpp
+++ source/movimentos.hpp
@@ -4,3 +4,17 @@
 #include <string>
 #include <iterator>
 #include <limits>
+
+// função de movimento de troca de um cliente de uma rota
+// para um cliente de outra rota
+void swapOnebyOne()
+{
+
+}
+
+// função de movimento de troca de dois clientes de uma rota
+// para um cliente de outra rota
+void swapTwoboyOne()
+{
+
+}
